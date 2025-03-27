78e4e3412d371e12d52dae21042dff7e0d6da18a
AMANverma9118 <AMANverma9118359330@gmail.com>
2025-03-27 19:31:50 +0530

    Changes of name

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const schoolRoutes = require("./routes/SchoolRoutes");
+const schoolRoutes = require("./routes/schoolRoutes");
 
 const app = express();
 
