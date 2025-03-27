10e6df8dca60d2f08e8e052e623924496e57d447
AMANverma9118 <AMANverma9118359330@gmail.com>
2025-03-27 19:16:37 +0530

    Changes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const schoolRoutes = require("./routes/schoolRoutes");
+const schoolRoutes = require("./Routes/SchoolRoutes");
 
 const app = express();
 
